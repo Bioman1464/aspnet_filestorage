e429f3ee65b648bd72bebdf2d7a68ed7f04145c3
bioman1464 <d.gapchuk@iilab.ru>
2021-12-09 00:23:53 +0300

    feat(Api\Startup): add swagger & cors & json policy & cast npgsql context

--- OOPASU.Api/Startup.cs
+++ OOPASU.Api/Startup.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using OOPASU.Api.Data;
+using OOPASU.Api.Repository;
+using OOPASU.Domain;
 
 namespace OOPASU.Api
 {
@@ -25,8 +24,36 @@ namespace OOPASU.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(options =>
+                {
+                    options.UseNpgsql(
+                        Configuration.GetConnectionString("DefaultConnection")
+                    );
+                    options.UseNpgsql(b => b.MigrationsAssembly("OOPASU.Api"));
+                }
+            );
+            services.AddScoped<IDataContext>(provider => provider.GetService<DataContext>());
+            services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddControllers();
+            services.AddSwaggerGen();
+
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                });
+
+            //Set the limit to 256 MB on multipart body requests
+            services.Configure<FormOptions>(options => options.MultipartBodyLengthLimit = 268435456);
+
+            // Add default cors policy
+            services.AddCors(options =>
+                options.AddPolicy(
+                    "default",
+                    policy => { policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); }
+                )
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,8 +61,19 @@ namespace OOPASU.Api
         {
             if (env.IsDevelopment())
             {
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
                 app.UseDeveloperExceptionPage();
             }
+
+            //Enable swagger doc generator
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseCors("default");
 
             app.UseHttpsRedirection();
 
@@ -43,10 +81,7 @@ namespace OOPASU.Api
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
